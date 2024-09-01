569802dbd36448224a9415080c92cbb631e8b512
Jose Luis Izquierdo Hernandez <110314046+jocl0110@users.noreply.github.com>
2024-09-01 03:00:57 -0500

    Finishing the send email functionality with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import './App.css'
-import Navbar from './components/NavBar'
-import Main from './components/Main'
-import Footer from './components/Footer'
-const App = () => {
+// import "./App.css";
+import Main from "./components/Main";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SendEmail from "./components/send-email/SendEmail";
 
+
+const App = () => {
   return (
     <>
-      <Navbar />
-      <Main />
-      <Footer />
+      <Router>
+        <Routes>
+      
+          
+            <Route path="/" element={<Main />}></Route>
+            <Route path="/send-email" element={<SendEmail />}></Route>
+        
+      
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
